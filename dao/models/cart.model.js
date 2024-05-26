0d1c82684f4389a6f41956fbe67e73015614dc41
IairFei <feigelmaniair@gmail.com>
2024-05-25 21:58:56 -0300

    Segunda pre-entrega

--- dao/models/cart.model.js
+++ dao/models/cart.model.js
@@ -5,9 +5,14 @@ const cartCollection = "carts"
 //Definimos el Schema de los carritos
 const cartSchema = new mongoose.Schema({
     products: {
-        type: Array,
-        default: [],
+        //En esta linea, se relaciona la propiedad de product
+        type: [{product: {type: mongoose.Schema.Types.ObjectId, ref: "products"}, quantity: Number}] 
     }
 });
+//Middleware
+//Esto se utiliza para que al momento de hacer el find, nos mustre el producto y no unicamente el ObjectId
+cartSchema.pre("find", function (){
+    this.populate("products.product")
+})
 
 export const cartModel = mongoose.model(cartCollection, cartSchema);
